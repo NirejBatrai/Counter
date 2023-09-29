c634c1ef3cd5b6f2873c9d0ede00bccca1d19f03
Nirej <purampandey1234@gmail.com>
2023-09-30 00:00:18 +0700

    error fixed

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-let count  = 0;
+let count = 0;
 
 document.getElementById("decreaseBtn").onclick = function () {
-    count -=1;
+    count -= 1;
     document.getElementById("countLabel").innerHTML = count;
-    
 }
-document.getElementById("resetBtn").onclick = function () {
+
+document.getElementById("resetBtn").onclick = function () { // This is correct, "resetBtn" matches the HTML id
     count = 0;
     document.getElementById("countLabel").innerHTML = count;
-
 }
-document.getElementById("increaseBtn").onclick = function () {
-    count = 1;
-    document.getElementById("countLabel").innerHTML = count;
 
+document.getElementById("increaseBtn").onclick = function () {
+    count += 1;
+    document.getElementById("countLabel").innerHTML = count;
 }
